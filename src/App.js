9fe91a0fc0b8e895c9aeae73688856b3a9554757
artem <my@example.com>
2020-06-17 17:50:31 +0300

    add main components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { 
+  Grid
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+import Type from './components/type/Type'
+import Main from './components/main/main'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    'padding-top': '100px'
+  }
+}))
 
 const App = () => {
-  return(<div>Hello</div>)
+  const classes = useStyles()
+
+  return(
+    <Grid container direction='column' align='center' justify='center' spacing={2} className={classes.root}>
+      <Grid item xs={9}>
+        <Main />
+      </Grid>
+      <Grid item xs={9}>
+        <Type />
+      </Grid>
+    </Grid>
+  )
 }
 
 export default App
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react'
+
+const Main = () => {
+  return (
+    <div>main</div>
+  )
+}
+
+export default Main
