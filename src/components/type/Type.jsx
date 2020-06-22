82e664d8bd035668b44e82a67dba318627d57f18
artem <my@example.com>
2020-06-22 12:15:00 +0300

    add simple animation

--- src/components/type/Type.jsx
+++ src/components/type/Type.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import {
   Card,
   Grid,
+  Slide,
   Typography,
   CardContent
 } from '@material-ui/core'
@@ -20,9 +21,16 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />
+})
 
 const Type = () => {
   const classes = useStyles()
+  const [checked, setChecked] = useState(false)
+  useEffect(() => {
+    setChecked((prev) => !prev);
+  }, [])
 
   const [types, setTypes] = useState([])
   useEffect(() => {
@@ -35,17 +43,20 @@ const Type = () => {
         Object.entries(types).map(([key, value]) => {
           return(
             <Grid item xs={6} key={key}>
-              <Card className={classes.card}>
-                <CardContent>
-                  <Typography align='left' variant='h5' component='h4'>
-                    { value.name }                  
-                  </Typography>
-                  {
-                    value.probability.map((item, idx) => (
-                      <TypeProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
-                  ))}
-                </CardContent>
-              </Card>
+              <Slide direction="up" in={checked} mountOnEnter unmountOnExit>
+                <Card className={classes.card}>
+                  <CardContent>
+                    <Typography align='left' variant='h5' component='h4'>
+                      { value.name }                  
+                    </Typography>
+                    {
+                      value.probability.map((item, idx) => (
+                        <TypeProgressBar key={idx} bgcolor={item.bgcolor} completed={item.completed} />
+                      ))
+                    }
+                  </CardContent>
+                </Card>
+              </Slide>
             </Grid>
           )
         })
