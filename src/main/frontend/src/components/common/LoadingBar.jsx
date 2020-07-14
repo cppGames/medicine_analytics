65ebb82974f0e0dc7a27786df3b628dcfbce4181
artem <my@example.com>
2020-07-14 12:20:18 +0300

    prepare custom loading bar

--- src/main/frontend/src/components/common/LoadingBar.jsx
+++ src/main/frontend/src/components/common/LoadingBar.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
-const LoadingBar = () => {
+
+const styles = (theme) => ({
+  root: {
+    position: 'relative',
+  },
+  bottom: {
+    color: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+  },
+  top: {
+    color: '#4ebfd1',
+    animationDuration: '950ms',
+    position: 'absolute',
+    left: 0,
+  },
+  circle: {
+    strokeLinecap: 'round',
+  },
+})
+
+const LoadingBar = (props) => {
+  const { classes } = props  
+
   return (
-    <div></div>
+    <div className={classes.root}>
+      <CircularProgress
+        variant="determinate"
+        className={ classes.bottom }
+        size={40}
+        thickness={4}
+        value={100}
+      />
+      <CircularProgress
+        variant="indeterminate"
+        disableShrink
+        className={ classes.top }
+        classes={{
+          circle: classes.circle,
+        }}
+        size={40}
+        thickness={4}
+      />
+    </div>
   )
 }
 
-export default LoadingBar
+export default withStyles(styles)(LoadingBar)
